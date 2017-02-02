b09525afc194592ae74afa37962f718d32f3053e
Sean <sean@seamonster.co.za>
2017-02-02 16:50:43 +0200

    Refactored login/registration process to accomodate password hashing and salting.

--- android/android_adduser.php
+++ android/android_adduser.php
@@ -4,16 +4,17 @@ require_once $_SERVER['DOCUMENT_ROOT']."/android/android_dbconnect.php";
 $name = $_POST["user"];
 $user_name = $_POST["user_name"];
 $user_password = $_POST["user_pass"];
+$user_salt = $_POST["salt"];
 
-$sql_query = "INSERT into user_info VALUES('$name', '$user_name', '$user_password');";
+$sql_query = "INSERT into user_info VALUES('$name', '$user_name', '$user_password', '$user_salt');";
 
 if (mysqli_query($conn, $sql_query))
 {
-	//echo "<h3> Data Insertion Success...</h3>";
+	echo "User successfully registered...";
 }
 else
 {
-	//echo "Data Insertion Error...".mysqli_error($conn);
+	echo "Data Insertion Error...".mysqli_error($conn);
 }
 
 ?>
--- android/android_login.php
+++ android/android_login.php
@@ -3,20 +3,53 @@ require_once $_SERVER['DOCUMENT_ROOT']."/android/android_dbconnect.php";
 
 $user_name = $_POST["login_name"];
 $user_password = $_POST["login_pass"];
+$user_salt = $_POST["login_salt"];
 
-$sql_query = "SELECT name FROM user_info WHERE user_name LIKE '$user_name' AND user_pass LIKE '$user_password';";
-
-$result = mysqli_query($conn, $sql_query);
-
-if (mysqli_num_rows($result) > 0)
+if ($user_salt == "")
 {
-	$row = mysqli_fetch_assoc($result);
-	$name = $row["name"];
-	echo "Login Success... Welcome ".$name;
+	// If no salt is provided on login, we need to fetch it to compare. (This is to accomodate a 2ndary request to the same script)
+	$getuser_query = "SELECT name FROM user_info WHERE user_name LIKE '$user_name';";
+
+	$result = mysqli_query($conn, $getuser_query);
+
+	if (mysqli_num_rows($result) > 0)
+	{
+		$row = mysqli_fetch_assoc($result);
+		$salt = $row["salt"];
+		echo $salt;
+	}
+	else
+	{
+		echo "No matching salt query found for ".$user_name;
+	}
 }
 else
 {
-	echo "Login Failed...";
+	// Salt was provided, let us now compare the hashed passwords
+	$getuser_query = "SELECT name FROM user_info WHERE user_pass LIKE '$user_password';";
+
+	$result = mysqli_query($conn, $getuser_query);
+
+	if (mysqli_num_rows($result) > 0)
+	{
+		$row = mysqli_fetch_assoc($result);
+		$user_name = $row["user_name"];
+
+		if ($salt == $row["salt"])
+		{
+			echo "Login successful for: ".$user_name;
+		}
+		else
+		{
+			echo "Passwords match, but salt does not!";
+		}
+	}
+	else
+	{
+		echo "No matching pw query found for ".$user_name;
+	}
 }
+
+
 
 ?>
